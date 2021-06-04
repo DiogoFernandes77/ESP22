dec9c4100f45d0e5c9e58606dad79b3ea8e909cf
Diogo <diogo.fernandes77@ua.pt>
2021-06-04 18:57:57 +0100

    test test

--- FindThePlane/complete/src/test/java/com/example/findThePlane/MainTest.java
+++ FindThePlane/complete/src/test/java/com/example/findThePlane/MainTest.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
-class mainTest {
+class MainTest {
 
     @Autowired
     private FlightController controller;
