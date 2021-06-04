182d0cf160d8d8d04dd5ebe9053bf8a341205735
Diogo <diogo.fernandes77@ua.pt>
2021-06-04 19:13:39 +0100

    test test

--- FindThePlane/complete/src/test/java/com/example/findThePlane/MainTest.java
+++ FindThePlane/complete/src/test/java/com/example/findThePlane/MainTest.java
@@ -1,15 +1,19 @@
 package com.example.findThePlane;
 
 import com.example.kafka.kafka_demo.FlightController;
+import com.example.kafka.kafka_demo.KafkaDemoApplication;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-@SpringBootTest
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = KafkaDemoApplication.class)
 class MainTest {
 
     @Autowired
