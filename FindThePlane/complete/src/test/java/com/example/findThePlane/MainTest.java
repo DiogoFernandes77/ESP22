19f8c3f50f90f1300c0cae800fdbc2c4ee9269fc
Diogo <diogo.fernandes77@ua.pt>
2021-06-04 18:42:18 +0100

    jenkins file

---
+++ FindThePlane/complete/src/test/java/com/example/findThePlane/MainTest.java
@@ -0,0 +1,38 @@
+package com.example.findThePlane;
+
+import com.example.kafka.kafka_demo.FlightController;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+@SpringBootTest
+class mainTest {
+
+    @Autowired
+    private FlightController controller;
+    
+    @Autowired
+	private MockMvc mockMvc;
+    
+    
+    @Test
+    void contextLoads() {
+        
+        System.out.println("Consumerdash Controller exists");
+        assertNotNull(controller);   
+    
+    }
+
+
+
+
+
+
+
+
+
+}
